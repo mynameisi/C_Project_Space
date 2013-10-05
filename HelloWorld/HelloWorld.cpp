92bfda888dbe92d8d899e2bfeb32132600a7cdea
mynameisi <mynamestillisi@gmail.com>
2013-10-05 18:13:34 +0800

    加了if

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 main(){
-	printf("Hello World1");
-	printf("Hello World!");
-	printf("支持中文么?");
+	printf("Hello World1\n");
+	printf("Hello World!\n");
+	printf("支持中文么?\n");
+	
+	int i=3;
+	if(i==3){
+		printf("i 等于 3\n");
+	}
 }
